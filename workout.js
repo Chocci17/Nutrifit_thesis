d180f4e96f5670ea6fd8a749f83387180fc8c7e9
Chocci17 <165173185+Chocci17@users.noreply.github.com>
2025-12-14 21:09:17 +0800

    s

---
+++ workout.js
@@ -0,0 +1,4 @@
+// Workout page JavaScript - placeholder for future interactivity
+document.addEventListener('DOMContentLoaded', () => {
+  // Future JavaScript for workout page features can be added here
+});
