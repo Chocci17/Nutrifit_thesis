7fcc81e570c26d9eb71bc8564cb8ccb4d0ccf964
Chocci17 <165173185+Chocci17@users.noreply.github.com>
2025-12-14 20:48:55 +0800

    s

--- JAVA/workout.js
+++
@@ -1,4 +0,0 @@
-// Workout page JavaScript - placeholder for future interactivity
-document.addEventListener('DOMContentLoaded', () => {
-  // Future JavaScript for workout page features can be added here
-});
